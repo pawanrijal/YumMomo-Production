8610348000e156565182b90c5a78985323300d0e
rijalsawan <rijalsawan@gmail.com>
2023-07-14 15:00:40 -0500

    12

--- pages/api/addProducts.js
+++ pages/api/addProducts.js
@@ -1,4 +1,4 @@
-import Product from "../../models/product.js";
+import Product from "../../models/Product.js";
 import connectDb from "../../middleware/mongoose.js";
 
 const handler = async (req, res) => {
